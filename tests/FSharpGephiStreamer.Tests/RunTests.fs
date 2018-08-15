9913491e058d69bb4f4690f62adef75867858966
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2018-08-15 11:17:55 +0200

    Change to new ProjectScaffold

---
+++ tests/FSharpGephiStreamer.Tests/RunTests.fs
@@ -0,0 +1,13 @@
+namespace FSharpGephiStreamer.Tests
+
+open Expecto
+
+module RunTests =
+
+    [<EntryPoint>]
+    let main args =
+
+        Tests.runTestsWithArgs defaultConfig args Tests.testSimpleTests |> ignore
+
+        0
+
